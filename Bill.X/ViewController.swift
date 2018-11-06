aa687f6ab286e47e27f9965b78b92233408ce03c
rocky <heng.yang@2339.com>
2018-11-06 14:04:11 +0800

    Initial Commit

---
+++ Bill.X/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bill.X
+//
+//  Created by meme-rocky on 2018/11/6.
+//  Copyright © 2018 meme-rocky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Bill.XTests/Bill_XTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bill_XTests.swift
+//  Bill.XTests
+//
+//  Created by meme-rocky on 2018/11/6.
+//  Copyright © 2018 meme-rocky. All rights reserved.
+//
+
+import XCTest
+@testable import Bill_X
+
+class Bill_XTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
